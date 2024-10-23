247644f0c66a454fdf24dffc304f9812b42a3166
JoshuaWilsonBlack <black.joshuad@gmail.com>
2024-10-23 15:42:35 +1300

    Smallcaps adding improved for qmd files.

--- R/smallcaps.R
+++ R/smallcaps.R
@@ -1,33 +1,45 @@
+#' @importFrom stringr str_extract
 smallcaps <- function() {
 
   # Get currently selected ranges.
   doc_selections <- rstudioapi::getSourceEditorContext()['selection']
+
+  # Determine prefix and postfix. For qmd files, use [text](.smallcaps),
+  # otherwise use <span style="font-variant: small-caps;">text</span>.
+  current_path <- rstudioapi::getSourceEditorContext()['path'][[1]]
+
+  extension <- str_extract(current_path, "\\.([A-Za-z]+)$", group=1)
+
+  if (extension == "qmd") {
+    pre <- "["
+    post <- "](.smallcaps)"
+  } else {
+    pre <- '<span style="font-variant: small-caps;">'
+    post <- '</span>'
+  }
 
   for (i in seq_along(doc_selections$selection)) {
     # Add text
     rstudioapi::modifyRange(
       location=doc_selections$selection[[i]]$range,
       text=base::paste0(
-        '<span style="font-variant: small-caps;">',
+        pre,
         doc_selections$selection[[i]]$text,
-        '</span>'
+        post
       )
     )
   }
 
-  # rstudioapi::setCursorPosition(
-  #   rstudioapi::document_position(
-  #     row=1,
-  #     column=1)
-  #   )
-
-
+  # Now modify selection to account for addition of pre.
   doc_ranges <- list()
 
   for (i in base::seq_along(doc_selections$selection)) {
 
-    shifted_start <- doc_selections$selection[[i]]$range$start + c(0, 40)
-    shifted_end <- doc_selections$selection[[i]]$range$end + c(0, 40)
+    shifted_start <- doc_selections$selection[[i]]$range$start +
+      c(0, nchar(pre))
+
+    shifted_end <- doc_selections$selection[[i]]$range$end +
+      c(0, nchar(pre))
 
     new_range = rstudioapi::document_range(
       start = rstudioapi::document_position(
