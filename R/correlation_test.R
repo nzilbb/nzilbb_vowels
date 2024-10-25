0741fc4956e9d6c2628015e78975ec180ef2a7be
JoshuaWilsonBlack <black.joshuad@gmail.com>
2024-10-25 16:38:28 +1300

    Improve documentation for `correlation_test` and add tests.

--- R/correlation_test.R
+++ R/correlation_test.R
@@ -1,13 +1,16 @@
-#' Run permutation test on correlations in a dataset.
+#' Permutation test of pairwise correlations
 #'
 #' Permute data a given number (n) of times, collecting pairwise correlations
-#' and testing them for significance.
+#' and testing them for significance. See [plot_correlation_magnitudes()] and
+#' [plot_correlation_counts()] for plotting functions which take the output of
+#' this function.
 #'
-#' @param pca_data data fed to the `prcomp` function. Remove non-continuous variables.
-#' @param n the number of times to permute that data. **Warning:** high values
-#'   will take a long time to compute.
+#' @param pca_data dataframe or matrix containing only continuous variables.
+#'   (as accepted by the `prcomp` function.)
+#' @param n the number of times (integer) to permute that data. **Warning:**
+#'   high values will take a long time to compute. Default: 100.
 #' @param cor.method method to use for correlations (default = "pearson").
-#'   Alternative is "spearman".
+#'   Alternative is "spearman" (see `?cor.test`).
 #' @returns object of class `correlation_test`, with attributes:
 #' * `$permuted_correlations` A tibble of length n of pairs from the original
 #' data, their correlations, and the significance of each correlation (as
@@ -17,27 +20,42 @@
 #' * `$iterations` the number of permutations carried out.
 #' * `$cor_method` the form of correlation used.
 #' @importFrom dplyr mutate filter across pull n
-#' @importFrom magrittr %>%
 #' @importFrom purrr map2 map_dbl
 #' @importFrom stats cor.test
 #' @importFrom tibble tibble as_tibble
 #' @importFrom tidyr expand
 #' @importFrom tidyselect everything
+#' @importFrom magrittr %>%
 #' @importFrom glue glue
 #' @importFrom stringr str_c
 #' @examples
-#' \dontrun{correlation_test(pca_data, n = 100, cor.method = 'pearson')}
+#'   # get a small sample of random intercepts.
+#'   pca_data <- onze_intercepts |>
+#'     select(-speaker) |>
+#'     slice_sample(n=10)
+#'
+#'   # apply correlation test with 10 permutations.
+#'   # actual use will require at least 100.
+#'   cor_test <- correlation_test(pca_data, n = 10, cor.method = 'pearson')
+#'   summary(cor_test)
+#'
+#'   # use spearman correlation instead.
+#'   cor_test_spear <- correlation_test(pca_data, n = 10, cor.method = 'spearman')
 #' @export
 correlation_test <- function(pca_data, n = 100, cor.method = 'pearson') {
 
-  original_pairs <- pca_data %>%
-    get_pairs() %>%
+  stopifnot(
+    "n must be a positive integer." = (n%%1 == 0 & n >= 1)
+  )
+
+  original_pairs <- pca_data |>
+    get_pairs() |>
     get_correlations(pca_data = pca_data, cor.method = cor.method)
 
 
   permed_pairs <- map(
     1:n,
-    ~ pca_data %>%
+    ~ pca_data |>
       # permute data
       mutate(
         across(
@@ -52,7 +70,7 @@ correlation_test <- function(pca_data, n = 100, cor.method = 'pearson') {
       )
   )
 
-  permed_pairs <- permed_pairs %>%
+  permed_pairs <- permed_pairs |>
     bind_rows(.id = "iteration")
 
   correlation_test <- list(
@@ -69,25 +87,25 @@ correlation_test <- function(pca_data, n = 100, cor.method = 'pearson') {
 }
 
 # Helper function to generate tibble with each pair from the original data
-# frame. Each pair only appears once.
+# frame. Each pair appears only once (i.e. these are unordered pairs).
 get_pairs <- function(pca_data) {
-  original_pairwise <- as_tibble(base::names(pca_data)) %>%
-    expand(.data$value, value1 = .data$value) %>%
+  original_pairwise <- as_tibble(base::names(pca_data)) |>
+    expand(.data$value, value1 = .data$value) |>
     filter(.data$value < .data$value1)
 }
 
 # Helper function to collect correlations for each pair and their p-values.
 get_correlations <- function(pair_names, pca_data, cor.method) {
 
-  out_data <- pair_names %>%
+  out_data <- pair_names |>
     mutate(
       htest = map2(
         .data$value,
         .data$value1,
         ~ cor.test(
-          pca_data %>%
+          pca_data |>
             pull(.x),
-          pca_data %>%
+          pca_data |>
             pull(.y),
           method=cor.method,
           exact=FALSE # Only active is spearman chosen.
@@ -95,14 +113,15 @@ get_correlations <- function(pair_names, pca_data, cor.method) {
       ),
       cor = map_dbl(.data$htest, ~ .x[['estimate']][[1]]),
       cor_p = map_dbl(.data$htest, ~.x[['p.value']])
-    ) %>%
-    select(-.data$htest)
+    ) |>
+    select(-"htest")
 
   out_data
 
 }
 
-#' Summary function for correlation test object. Set alpha to change significance
+#' Summary function for correlation test object. Set alpha to change
+#' significance level.
 #'
 #' Set alpha to change significance level and n_cors to change number of pairwise
 #' correlations given.
@@ -123,21 +142,21 @@ get_correlations <- function(pair_names, pca_data, cor.method) {
 summary.correlation_test <- function(object, alpha = 0.05, n_cors = 5, ...) {
 
 
-  original_cor_count <- object$original_correlations %>%
-    filter(.data$cor_p < alpha) %>%
+  original_cor_count <- object$original_correlations |>
+    filter(.data$cor_p < alpha) |>
     nrow()
 
-  perm_sum <- object$permuted_correlations %>%
-    filter(.data$cor_p < alpha) %>%
-    group_by(.data$iteration) %>%
+  perm_sum <- object$permuted_correlations |>
+    filter(.data$cor_p < alpha) |>
+    group_by(.data$iteration) |>
     summarise(
       n = n()
-    ) %>%
-    pull(.data$n) %>%
+    ) |>
+    pull(.data$n) |>
     base::summary()
 
-  top_n <- object$original_correlations %>%
-    arrange(desc(base::abs(.data$cor))) %>%
+  top_n <- object$original_correlations |>
+    arrange(desc(base::abs(.data$cor))) |>
     utils::head(n_cors)
 
 
